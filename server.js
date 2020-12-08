4a38f3397d20897d24c7e299dda9e03b2dfddb38
ferranPZ <lucas.pendola.z@gmail.com>
2020-12-07 21:58:21 -0300

    server update

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
-const express = require("express");
-const app = expres();
-const path = require('path')
+const express = require('express');
+const path = require('path');
+const app = express();
 
-app.use(express.static(_dirname + '/dist'));
-
-app.listen(process.env.PORT || 8080);
-
-
+app.use(express.static(__dirname + '/dist/colombia'));
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-})
-
-
-console.log('console listening!')
+    res.sendFile(path.join(__dirname +
+        '/dist/colombia/index.html'));
+});
+app.listen(process.env.PORT || 8080);
